abdd1d623dcb40480cf7548824a99e7799a79b3a
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-14 23:14:10 +0300

    add methode and button clear data and add map from Expenses.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,36 +2,64 @@ import "./ExpenseForm.css";
 import {useState} from "react";
 
 
-
 const ExpenseForm = () => {
 
     const [title, setTitle] = useState('');
-    const [price, setPrice] = useState(0.1);
-    const [date, setDate] = useState(new Intl.DateTimeFormat('en-GB', { dateStyle: 'medium'}).format(new Date));
+    const [price, setPrice] = useState(1);
+    const [date, setDate] = useState(new Intl.DateTimeFormat('en-GB', {dateStyle: 'medium'}).format(new Date));
 
+    // add expense value in array
+    const add_expense = () => {
+        console.log(title);
+        console.log(price);
+        console.log(date)
+    }
+
+    // Clear all input in Expense Form
+    const clear_expense = ()=> {
+        setTitle('');
+        setPrice(1);
+        setDate(new Date());
+    }
 
     return (
 
-    <form>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" />
-        </div>
-        <div className="new-expense__control">
-          <label>Price</label>
-          <input type="number" min="1" step="0.1" />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type="date" min="2022-01-01" max="2022-12-31" />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
-  );
+        <>
+
+
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" value={title} onChange={(e) => {
+                        setTitle(e.target.value)
+                    }}/>
+                </div>
+
+                <div className="new-expense__control">
+                    <label>Price</label>
+                    <input type="number" min="1" step="0.1"
+                           value={price} onChange={(e) => {
+                        setPrice(parseFloat(e.target.value))
+                    }}/>
+                </div>
+
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2022-01-01" max="2022-12-31"
+                           value={date} onChange={(e) => {
+                        setDate(e.target.value)
+                    }}/>
+                </div>
+            </div>
+
+            <div className="new-expense__actions">
+                <button onClick={add_expense}>Add Expense</button>
+                <button onClick={clear_expense}>Clear Expense</button>
+            </div>
+
+
+        </>
+    );
 };
 
 export default ExpenseForm;
