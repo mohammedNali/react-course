2ba7a966d86f1c3d54150fe298d1d387b30a31b2
M7MMAD-OMAR <73592103+M7MMAD-OMAR@users.noreply.github.com>
2022-09-15 10:01:59 +0300

    add methode convert month number to month name and fix problem date

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -3,14 +3,24 @@ const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear
-
+    const temp = props.date.toString().split(',')
   return (
     <div className='expense-date'>
-      <div className='expense-date__month'>{month}</div>
-      <div className='expense-date__year'>{year}</div>
-      {/*<div className='expense-date__day'>{day}</div>*/}
+      <div className='expense-date__month'>{temp[0]}</div>
+      <div className='expense-date__year'>{toMonthName(temp[1])}</div>
+      <div className='expense-date__day'>{temp[2]}</div>
     </div>
   );
+}
+
+// convert month number to month name
+const toMonthName = (monthNumber)=> {
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+
+    return date.toLocaleString('en-US', {
+        month: 'long',
+    });
 }
 
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,32 +1,27 @@
 import "./ExpenseForm.css";
 import {useState} from "react";
 
-const longEnUSFormatter = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-})
 
 const ExpenseForm = () => {
     let counter = 1;
-    const date = longEnUSFormatter.format(new Date());
-    const [title_temp, setTitle] = useState('');
-    const [price_temp, setPrice] = useState(1);
-    const [date_temp, setDate] = useState(date);
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState(1);
+    let [date, setDate] = useState(new Date().toDateString());
     const [products, setProduct] = useState(JSON.parse(localStorage.getItem('products') || '[]'));
 
-
-    console.log(date_temp)
-
+    // convert date to array
+    date = date.toString().split('-')
+    console.log(date)
 
     // add expense value in array
     const add_expense = () => {
         const temp = {
             id: counter++,
-            title: title_temp,
-            price: price_temp,
-            date: date_temp,
+            title: title,
+            price: price,
+            date: date,
         }
+
         products.push(temp);
         setProduct(products);
         localStorage.setItem('products', JSON.stringify(products));
@@ -37,7 +32,6 @@ const ExpenseForm = () => {
     const clear_expense = ()=> {
         setTitle('');
         setPrice(1);
-        setDate(new Date());
     }
 
     return (
@@ -48,7 +42,7 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" value={title_temp} onChange={(e) => {
+                    <input type="text" value={title} onChange={(e) => {
                         setTitle(e.target.value)
                     }}/>
                 </div>
@@ -56,7 +50,7 @@ const ExpenseForm = () => {
                 <div className="new-expense__control">
                     <label>Price</label>
                     <input type="number" min="1" step="0.1"
-                           value={price_temp} onChange={(e) => {
+                           value={price} onChange={(e) => {
                         setPrice(parseFloat(e.target.value))
                     }}/>
                 </div>
@@ -64,7 +58,7 @@ const ExpenseForm = () => {
                 <div className="new-expense__control">
                     <label>Date</label>
                     <input type="date" min="2022-01-01" max="2022-12-31"
-                           value={date_temp} onChange={(e) => {
+                           onChange={(e) => {
                         setDate(e.target.value)
                     }}/>
                 </div>
